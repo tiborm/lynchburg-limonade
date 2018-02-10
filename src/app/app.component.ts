ab1bc5191c626fed0083b3116a60a682e5301bb1
Tibor Meller <tibor.meller@gmail.com>
2018-02-10 21:26:52 +0100

    Resolve #3 - adding search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lyli-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Lynchburg Limonade';
+  subTitle = 'Wish I could use Angular more often...';
 }
