ee004682503291a8a34fc78dada41891362e4b16
Tibor Meller <tibor.meller@gmail.com>
2018-02-10 21:42:39 +0100

    [#4] adding model

---
+++ src/app/models/repo.ts
@@ -0,0 +1,11 @@
+export interface Repo {
+  id: number;
+  name: string;
+  full_name: string;
+  stargazers_count: number;
+  watchers_count: number;
+  language: string;
+  forks_count: number;
+  open_issues_count: number;
+  score: number;
+}
